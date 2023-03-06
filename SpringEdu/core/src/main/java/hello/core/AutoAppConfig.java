9fa6af1e138eef052591d893a7aa04fc6c2b8727
whxogus215 <whxogus215@naver.com>
2023-03-06 23:28:52 +0900

    필터

--- SpringEdu/core/src/main/java/hello/core/AutoAppConfig.java
+++ SpringEdu/core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         // @Configuration이 붙은 것들은 제외하기 (충돌 방지 - 중복)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // AppConfig는 항상 프로젝트의 최상단에 두기 (컴포넌트 스캔 대상 : AppConfig를 기준으로 함)
 )
 public class AutoAppConfig {
 
