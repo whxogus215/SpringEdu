3b21e8c5d12e2d9e9871173430bf9528a2a291f5
whxogus215 <whxogus215@naver.com>
2023-03-09 00:28:06 +0900

    ~롬복과 최신 트렌드

--- SpringEdu/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringEdu/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,21 +4,17 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     // 철저하게 인터페이스만 의존하고 있음, 구현체가 어떤 것으로 올지 전혀 알 수가 없음.
     private final MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final DiscountPolicy discountPolicy; // final을 사용하면 기본 값을 할당하거나 생성자를 만들어야 함, 인터페이스만 의존하도록 함, 관심사 분리를 위해서 구현체를 선언하지 않음
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = this.memberRepository.findById(memberId);
