7b2013f3cf35497b1e9c41656e012c25d3d563f9
whxogus215 <whxogus215@naver.com>
2023-06-05 07:07:06 +0900

    Add 코드 설명

--- SpringEdu/mvc/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
+++ SpringEdu/mvc/servlet/src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class ModelView {
     private String viewName; // View의 논리적인 이름
-    private Map<String, Object> model = new HashMap<>(); // 뷰 렌더링에 필요한 model 객체 -> JSP에서 값을 가져오는 곳
+    private Map<String, Object> model = new HashMap<>(); // 뷰 렌더링에 필요한 Map 저장소 -> JSP에서 값을 가져오는 곳
 
     public ModelView(String viewName) {
         this.viewName = viewName;
