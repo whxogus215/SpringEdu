907d9c1d36f80ec83caade342c022121b106656a
whxogus215 <whxogus215@naver.com>
2023-06-15 07:11:23 +0900

    로깅 간단히 알아보기 ~ 15분 50초

---
+++ SpringEdu/mvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok"; // @RestController의 메서드이기 때문에 단순히 문자열이 그대로 반환된다. (View의 논리적 이름이 아님)
+
+    }
+
+
+}
