46d12489b41d484959a56bb86e053d8ceb5fb272
whxogus215 <whxogus215@naver.com>
2023-06-23 10:32:23 +0900

    ~RedirectAttribute

--- SpringEdu/mvc/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ SpringEdu/mvc/item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -29,7 +30,7 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "basic/item";
+        return "basic/item"; // PathVariable의 경우, 같은 View를 반환하지만, 동적 템플릿이 적용된다. -> 하나의 View를 통해 쉽게 구현이 가능(장점)
     }
 
     @GetMapping("/add")
@@ -65,11 +66,29 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV4(Item item) { // 객체가 파라미터로 들어올 경우, 어노테이션을 생략하면 @ModelAttribute가 자동으로 추가된다!
 
         itemRepository.save(item);
-        return "basic/item";
+        return "basic/item"; // 문제점 : PRG 패턴이 적용되지 않아, 이 상태에서 새로고침할 경우 POST 요청이 또 된다. -> Redirect 필요
+    }
+
+    // @PostMapping("/add")
+    public String addItemV5(Item item) {
+
+        itemRepository.save(item);
+//        return "basic/item";
+        // POST 요청이 끝나면 POST의 View를 반환하는 것이 아니라 GET 요청을 받는 다른 URL로 Redirect한다.
+        // 이러면 사용자가 다시 새로고침을 해도 최근 요청이 GET이기 때문에 POST 요청의 중복 문제가 발생하지 않는다.
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; // PathVariable로 들어가지 못한 값들은 쿼리 파라미터로 자동으로 들어가게 된다.
     }
 
     @GetMapping("/{itemId}/edit")
