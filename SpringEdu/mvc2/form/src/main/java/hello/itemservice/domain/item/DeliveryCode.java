0fbef0f525f698f6cad84e0133c27be0dd74e413
whxogus215 <whxogus215@naver.com>
2023-06-30 11:17:20 +0900

    섹션 2 : ~ 요구사항 추가

---
+++ SpringEdu/mvc2/form/src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,18 @@
+package hello.itemservice.domain.item;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * FAST : 빠른 배송
+ * NORMAL : 일반 배송
+ * SLOW : 느린 배송
+ */
+@Data
+@NoArgsConstructor // 생성자가 없는 파라미터를 자동 생성
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+}
