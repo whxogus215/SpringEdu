f2cb05322a0164a555dae5012fbbb8780d54abd0
whxogus215 <whxogus215@naver.com>
2023-07-01 09:49:58 +0900

    ~ 체크박스 : 멀티

--- SpringEdu/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ SpringEdu/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,19 +3,34 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions") // 어떤 컨트롤러를 호출하더라도 이 값이 모델에 자동으로 담기게 됨
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    } // 컨트롤러가 호출될 때마다 이 메서드도 계속 호출이 되기 때문에 하나의 static으로 빼두는 방법도 고려할만하다.
+
+
 
     @GetMapping
     public String items(Model model) {
@@ -39,6 +54,10 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        log.info("item.open = {}", item.getOpen());
+        log.info("item.regions = {}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -54,6 +73,9 @@ public class FormItemController {
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+
+        log.info("item.open = {}", item.getOpen());
+
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
