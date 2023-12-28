8eae42b9b0417a0295df65972a3a333faec5bc20
whxogus215 <whxogus215@naver.com>
2023-12-28 11:20:02 +0900

    섹션 3 : 준영속 상태

--- SpringEdu/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ SpringEdu/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member(200L, "test");
-            em.persist(member);
-            
-            em.flush(); // 직접 flush를 호출하게 되면, 쓰기 지연 SQL 저장소에 있는 쿼리문이 바로 DB에 나가게 됨
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("AAAA");
 
-            tx.commit(); // 실제 DB에 영구적으로 저장되는 것은 커밋 이후
+            em.detach(findMember); // 엔티티 매니저에서 관리하지 않게 만듦(준영속화)
+            // em.clear(); 그 외에 준영속화를 할 수 있는 메서드들
+            // em.close();
+
+            tx.commit(); // 커밋 시 UPDATE 쿼리 안나감
         } catch (Exception e) {
             tx.rollback();
         } finally {
