44aa2d13d3b8c59c53556096422b6ce5b9e93b0f
whxogus215 <whxogus215@naver.com>
2023-12-28 11:12:41 +0900

    섹션 3 : 플러시
    - em.flush()를 통해 직접 플러시 호출 : 쿼리문을 확인하기 위해 테스트할 때 주로 사용됨
    - 트랜잭션 커밋 시 자동으로 플러시가 호출됨
    - JPQL 쿼리 실행 시 자동으로 플러시가 호출됨
    
    - 플러시는 엔티티 매니저 내에 있는 1차 캐시를 비우는 것이 아님. 단지 쓰기 지연 SQL 저장소에 쌓여 있는 쿼리문을 DB로 날리는 것을 의미!

--- SpringEdu/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ SpringEdu/jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 150L);
-            findMember.setName("ZZZZ");
+            Member member = new Member(200L, "test");
+            em.persist(member);
+            
+            em.flush(); // 직접 flush를 호출하게 되면, 쓰기 지연 SQL 저장소에 있는 쿼리문이 바로 DB에 나가게 됨
 
-            // em.persist(findMember); 엔티티 수정 시 다시 영속화 하지 않는다! 별도의 update 메서드도 없음
-
-            tx.commit();
+            tx.commit(); // 실제 DB에 영구적으로 저장되는 것은 커밋 이후
         } catch (Exception e) {
             tx.rollback();
         } finally {
